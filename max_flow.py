57404bf9914f49bf6b3e90845eb08f14da9c0f46
maganoegi <platserg94@yahoo.com>
2019-11-18 21:55:20 +0100

    algo works, and is separated

--- max_flow.py
+++ max_flow.py
@@ -3,45 +3,114 @@
 #                   ----------------------------------------------------------------------
 #                   |             * PLATONOV Sergey         KALTRACHIAN Téo              |
 #                   |             * Algorithmes avancées                                 |
-#                   |             * Algorithmes de Ford-Fulkerson                        | 
+#                   |             * Algorithme de Ford-Fulkerson                         | 
 #                   |             * HEPIA ITI sem. 3            * max_flow.py            | 
 #                   ----------------------------------------------------------------------
 
 #!/usr/bin/python3
-import sys
+import mylib as mlib
 
 #============================================================#
 #                  Variables initialization                  #
 #============================================================#
 nb_nodes = 0
-nb_edges = 0
-capacity = 0
 splitted = []
 network_edges = []
-
+graph = []
 #============================================================#
 #              Take arguments from command-line              #
 #============================================================#
-input_file_name = sys.argv[1]
+# input_file_name = sys.argv[1]
+input_file_name = "input_network.txt"
 
 #============================================================# 
-#          Get and organize contents of the file             #
+#     Extract contents file     +     Populate matrix        #
 #============================================================#
-with open (input_file_name, "r") as f:
+with open(input_file_name, "r") as f:
     for index, line in enumerate(f):
+        # remove trailing \n in the end of the string
         line = line.rstrip()
+
+        # split string into array of substrings
         splitted = line.split(" ")
-        if index == 0:              
+
+        # first line is data about the flow network ...
+        if index == 0:  
             nb_nodes = int(splitted[0])
-            nb_edges = int(splitted[1])
-        else:
-            edge_vals = []
-            for i in range(3):
-                edge_vals.append(int(splitted[i]))
-            network_edges.append(edge_vals)
+            
+            # use the data to create a matrix filled with zeros
+            graph = mlib.generate_matrix_of_dimension(nb_nodes, nb_nodes)
+
+        # ... and the rest are edges, whose info we insert into the matrix
+        else:    
+            source = int(splitted[0]) - 1           #======================#         
+            destination = int(splitted[1]) - 1      #  Matrix:     0-based
+            capacity = int(splitted[2])             #  Nodes:      1-based
+                                                    #  hence, "-1" here     
+            graph[source][destination] = capacity   #======================#
+                                                    
+    f.close()
 
 #============================================================# 
-#                       Hidden Layer                         #
+#               Apply Ford-Fulkerson algorithm               #
 #============================================================#
 
+# create an instance of the graph class, using our matrix
+g = mlib.Graph(graph)
 
+# execute algorithm
+source = 0; sink = (nb_nodes - 1)
+maximum_flow = g.FordFulkerson(source, sink)
+
+
+#============================================================# 
+#                    Visualisation Results                   #
+#============================================================#
+print ("Max Flow of this network is = " + mlib.bcolors.GREEN + str(maximum_flow) + mlib.bcolors.ENDC)
+
+
+# 6 10
+# 1 2 16
+# 1 3 13
+# 2 3 10
+# 3 2 4
+# 2 4 12
+# 3 5 14     maxflow: 23 
+# 4 3 9
+# 5 4 7
+# 4 6 20
+# 5 6 4
+
+# 23 32     maxflow: 24
+# 1 2 10
+# 1 3 14
+# 2 13 25
+# 13 5 72
+# 13 20 15
+# 5 7 9
+# 5 14 18
+# 7 9 4
+# 14 17 24
+# 14 20 34
+# 9 11 42
+# 9 19 42
+# 11 19 50
+# 17 21 12
+# 20 22 14
+# 21 22 16
+# 21 23 25
+# 19 21 19
+# 19 23 16
+# 3 15 10
+# 3 4 11 
+# 4 15 12
+# 4 6 17
+# 15 6 19
+# 15 16 34
+# 6 18 35
+# 16 8 10
+# 8 22 16
+# 18 10 58
+# 10 12 11
+# 12 22 45
+# 22 23 32
---
+++ mylib.py
@@ -0,0 +1,119 @@
+
+
+#                   ----------------------------------------------------------------------
+#                   |             * PLATONOV Sergey         KALTRACHIAN Téo              |
+#                   |             * Algorithmes avancées                                 |
+#                   |             * Algorithme de Ford-Fulkerson                         | 
+#                   |             * HEPIA ITI sem. 3            * mylib.py               | 
+#                   ----------------------------------------------------------------------
+
+
+#============================================================#
+#                         Imports                            #
+#============================================================#
+from collections import defaultdict 
+import sys
+
+#============================================================#
+#                   Terminal Color Class                     #
+#============================================================#
+class bcolors:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+#============================================================#
+#                   Null Matrix Generation                   #
+#============================================================#
+def generate_matrix_of_dimension(width, height):
+    zero_line = []
+    zMatrix = []
+    for i in range(height):
+        zero_line = []
+        for j in range(width):
+            zero_line.append(0)
+        zMatrix.append(zero_line)
+    return zMatrix
+
+#============================================================#
+#          Graph Class - with corresponding methods          #
+#============================================================#
+# Organizes all the edge capacities, and allows operations 
+class Graph: 
+    def __init__(self, graph): 
+        # residual graph 
+        self.graph = graph 
+        self. ROW = len(graph) 
+   
+    # Returns true if there is a path from source 's' to sink 't' in 
+    # residual graph. Also fills parent[] to store path
+    def BFS(self,s, t, parent): 
+  
+        # Mark all the vertices as not visited 
+        visited = [False]*(self.ROW) 
+          
+        # Create a queue for BFS 
+        queue = [] 
+          
+        # Mark the source node as visited and enqueue it 
+        queue.append(s) 
+        visited[s] = True
+           
+         # Standard BFS Loop 
+        while queue: 
+  
+            # Dequeue a vertex from queue and print it 
+            u = queue.pop(0) 
+          
+            # Get all adjacent vertices of the dequeued vertex u 
+            # If a adjacent has not been visited, then mark it 
+            # visited and enqueue it 
+            for ind, val in enumerate(self.graph[u]): 
+                if visited[ind] == False and val > 0 : 
+                    queue.append(ind) 
+                    visited[ind] = True
+                    parent[ind] = u 
+  
+        # If we reached sink in BFS starting from source, then return 
+        # true, else false 
+        return True if visited[t] else False
+              
+      
+    # Returns tne maximum flow from s to t in the given graph 
+    def FordFulkerson(self, source, sink): 
+  
+        # This array is filled by BFS and to store path 
+        parent = [-1]*(self.ROW) 
+  
+        max_flow = 0 # There is no flow initially 
+  
+        # Augment the flow while there is path from source to sink 
+        while self.BFS(source, sink, parent) : 
+  
+            # Find minimum residual capacity of the edges along the 
+            # path filled by BFS. Or we can say find the maximum flow 
+            # through the path found. 
+            path_flow = float("Inf") 
+            s = sink 
+            while(s !=  source): 
+                path_flow = min(path_flow, self.graph[parent[s]][s]) 
+                s = parent[s] 
+  
+            # Add path flow to overall flow 
+            max_flow +=  path_flow 
+  
+            # update residual capacities of the edges and reverse edges 
+            # along the path 
+            v = sink 
+            while(v !=  source): 
+                u = parent[v] 
+                self.graph[u][v] -= path_flow 
+                self.graph[v][u] += path_flow 
+                v = parent[v] 
+  
+        return max_flow 
